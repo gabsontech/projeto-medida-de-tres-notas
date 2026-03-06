42424772cd58269a44fb2c9d2d426d8b380c1b65
gabsontech <kalzonefinals@gmail.com>
2026-03-06 13:28:18 -0300

    Add files via upload

---
+++ joao.py
@@ -0,0 +1,8 @@
+print("programa de fazer notas medias!!!!!!!")
+choice=int(input("fala uma das tres notas de um certo aluno!"))
+choice2=int(input("segunda nota?"))
+choice3=int(input("terceira nota!"))
+asd = choice+choice2+choice3
+dsd = asd // 3
+print("nota media desse aluno(a) é",dsd)
+
