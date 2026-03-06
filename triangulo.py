ff0accdf58f17ee83407e393dcacce5bf05fae71
gabsontech <kalzonefinals@gmail.com>
2026-03-06 14:07:27 -0300

    Add files via upload

---
+++ triangulo.py
@@ -0,0 +1,7 @@
+print("PROGRAMA DE MEDIR AREA DE TRIANGULO")
+choice=int(input("fala aí a base do triangulo"))
+choice2=int(input("fala aí a altura do triangulo"))
+asd=choice*choice2
+dih=asd // 2
+print("a área resenha do triangulo é",dih)
+
