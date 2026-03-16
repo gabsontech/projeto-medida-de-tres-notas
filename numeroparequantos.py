70ceb5e26539f9dafffd2135b52fb523b528183b
gabsontech <kalzonefinals@gmail.com>
2026-03-15 22:24:42 -0300

    Add files via upload

---
+++ numeroparequantos.py
@@ -0,0 +1,45 @@
+par = 0
+impar = 0
+num1denovo = 0
+num2denovo = 0
+num2denovo = 0
+num4denovo = 0
+num5denovo = 0
+print("MOSTRADOR DE QUANTOS SAO PARES")
+num1=int(input("fala ai o primeiro numero"))
+num2=int(input("fala ai o segundo numero"))
+num3=int(input("fala ai o terceiro numero"))
+num4=int(input("fala ai o quarto numero"))
+num5=int(input("fala ai o quinto numero"))
+num1denovo = num1 // 2 
+if num1denovo == int:
+    par = par + 1
+#if num1denovo == float:
+#    impar = impar + 1
+
+num2denovo = num2 // 2 
+if num2denovo == int:
+    par = par + 1
+#if num2denovo == float:
+#    impar = impar + 1
+
+num3denovo = num3 // 2 
+if num3denovo == int:
+    par = par + 1
+#if num3denovo == float:
+#    impar = impar + 1
+
+num4denovo = num4 // 2 
+if num4denovo == int:
+    par = par + 1
+#if num4denovo == float:
+#    impar = impar + 1
+
+num5denovo = num5 // 2 
+if num5denovo == int:
+    par = par + 1
+#if num5denovo == float:
+ #   impar = impar + 1
+print("de pares tem",par,", ja de impar tem",impar)
+    
+     
